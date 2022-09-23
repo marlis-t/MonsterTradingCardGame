e1cd30e184bb3fe89030cc4b461ac97d61fb5036
marlis-t <if21b152@technikum-wien.at>
2022-09-23 14:08:43 +0200

    card content defined

--- Monster-Trading-Card-Game/src/main/java/card/Card.java
+++ Monster-Trading-Card-Game/src/main/java/card/Card.java
@@ -1,4 +1,29 @@
 package card;
 
+import lombok.Getter;
+
 public class Card {
+    @Getter
+    private String name;
+    @Getter
+    private int damage;
+    @Getter
+    private boolean isPaused;
+    @Getter
+    private String type;
+    @Getter
+    private String element;
+    public void setIsPaused(boolean isPaused) {
+        this.isPaused = isPaused;
+    }
+
+    Card(String name, int damage, String type, String element){
+        this.name = name;
+        this.damage = damage;
+        this.isPaused = false;
+        this.type = type;
+        this.element = element;
+    }
+
+
 }
